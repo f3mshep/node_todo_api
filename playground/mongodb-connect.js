68b2b1cadd59e0f100c993d923622d586c7aba5d
f3mshep <superbiscuit@gmail.com>
2018-05-11 16:11:30 -0700

    node db connected

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
   if (err){
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp')
 
-  db.close();
+  // db.collection('Todos').insertOne({
+  //   text: "Do the thing",
+  //   completed: false
+  // }, (err, res)=> {
+  //   if (err) {
+  //     return console.log('Unable to insert ', err)
+  //   }
+
+  //   console.log(res.ops)
+  // });
+
+  db.collection('Users').insertOne({
+    name: "The Nameless One",
+    age: 9999,
+    location: "Sigil"
+  }, (err, res) => {
+    if (err) {
+      return console.log('Unable to insert', err);
+    };
+
+    console.log(res.ops);
+  });
+
+  client.close();
 });
 
