666460b7e12545e5c23066b96a076bd5022eed9f
f3mshep <superbiscuit@gmail.com>
2018-05-14 11:31:57 -0700

    post request success

--- server/server.js
+++ server/server.js
@@ -1,3 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongo');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
+const app = express();
+PORT = 3000
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  })
+  todo.save().then((doc)=>{
+    console.log("Document created:\n", doc)
+    res.send(doc);
+  }, (e)=>{
+    console.log("Bad request:\n", e)
+    res.status(400).send(e)
+  })
+});
+
+app.get('/todos', (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`)
+});
