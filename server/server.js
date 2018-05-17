c0c5d909cccdb83fa6c02a835661b2027183415a
f3mshep <superbiscuit@gmail.com>
2018-05-17 09:55:17 -0700

    fixed runtime error

--- server/server.js
+++ server/server.js
@@ -1,14 +1,5 @@
 require('./config/config');
 
-if (env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp"
-}
-else if (env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
-}
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require("mongodb");
